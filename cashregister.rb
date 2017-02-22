f862cf286b1774b2479b2e0a9f5c0d68fac8f1fc
michaelciletti <michaelciletti@gmail.com>
2017-02-22 15:02:36 -0500

    fifth test passes

--- cashregister.rb
+++ cashregister.rb
@@ -1,12 +1,14 @@
 def cashregister(amount)
 
- change = {}
+  change = {}
  if amount == 0 
  	{}
  elsif amount == 5 
  	change = {Nickels: 1}
- else amount < 5 
+ elsif amount < 5 
  change = {Pennies: amount}
+else amount == 6
+	change = {Nickels: 1, Pennies: 1}
 	
  end
 
