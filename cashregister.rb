490133d6a83da46746bfb947ecd207dea9fdcc84
michaelciletti <michaelciletti@gmail.com>
2017-02-22 17:30:54 -0500

    test 7 now works

--- cashregister.rb
+++ cashregister.rb
@@ -1,11 +1,11 @@
 def cashregister(amount)
 
   change = {}
- 	if amount == 10 
-		change = {Dimes: 1}
-	elsif amount > 10 
+ 	if amount > 10 
 		change[:Dimes] = 1 
 		change[:Pennies] = amount - 10
+	elsif amount == 10 
+		change = {Dimes: 1}
 	change = {Dimes: 1}
  	elsif	amount == 0 
  		{}
@@ -15,7 +15,7 @@ def cashregister(amount)
  		change = {Pennies: amount}
 	# elsif amount == 6
 	# 	change = {Nickels: 1, Pennies: 1}
-	elsif amount > 5 
+	else amount > 5 
 		change[:Nickels] = 1 
 		change[:Pennies] = amount - 5
 	
