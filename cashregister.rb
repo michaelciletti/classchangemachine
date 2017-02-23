2fdefd5b98def7e8b3d58a2fb6de6e5a8dc32ecd
michaelciletti <michaelciletti@gmail.com>
2017-02-22 19:41:55 -0500

    set coins equal to their values

--- cashregister.rb
+++ cashregister.rb
@@ -1,35 +1,40 @@
 def cashregister(amount)
-
+	
+	quarters = 25
+	dimes = 10
+	nickels = 5
+	pennies = 1
+	
   change = {}
+ 	
+
  	if amount > 25 && amount < 30
-		change[:Quarters] = 1 
-		change[:Pennies] = amount - 25
+		change[:quarters] = 1 
+		change[:pennies] = amount - 25
  	elsif amount == 25 
-		change = {Quarters: 1}
+		change = {quarters: 1}
  	elsif amount >= 20 && amount < 25
- 		change[:Dimes] = 2
- 		change[:Pennies] = amount - 20
+ 		change[:dimes] = 2
+ 		change[:pennies] = amount - 20
  	elsif amount > 15 && amount < 20
-		change[:Dimes] = 1 
-		change[:Nickels] = 1
-		change[:Pennies] = amount - 15
+		change[:dimes] = 1 
+		change[:nickels] = 1
+		change[:pennies] = amount - 15
  	elsif amount > 10 && amount < 15
-		change[:Dimes] = 1 
-		change[:Pennies] = amount - 10
+		change[:dimes] = 1 
+		change[:pennies] = amount - 10
 	elsif amount == 10 
-		change = {Dimes: 1}
-	change = {Dimes: 1}
+		change = {dimes: 1}
+	change = {dimes: 1}
  	elsif	amount == 0 
  		{}
  	elsif amount == 5 
- 		change = {Nickels: 1}
+ 		change = {nickels: 1}
  	elsif amount < 5 
- 		change = {Pennies: amount}
-	# elsif amount == 6
-	# 	change = {Nickels: 1, Pennies: 1}
+ 		change = {pennies: amount}
 	else amount > 5 
-		change[:Nickels] = 1 
-		change[:Pennies] = amount - 5
+		change[:nickels] = 1 
+		change[:pennies] = amount - 5
 	
 		
 		
--- test_cashregister.rb
+++ test_cashregister.rb
@@ -9,43 +9,43 @@ class TestCashregister < Minitest::Test
 	end
 
 	def test_cr_pass_1_returns_1_pennies
-		assert_equal({Pennies: 1}, cashregister(1))
+		assert_equal({pennies: 1}, cashregister(1))
 	end
 
 	def test_cr_pass_2_returns_2_pennies
-		assert_equal({Pennies: 2}, cashregister(2))
+		assert_equal({pennies: 2}, cashregister(2))
 	end
 
 	def test_cr_pass_5_returns_1_nickel
-		assert_equal({Nickels: 1}, cashregister(5))
+		assert_equal({nickels: 1}, cashregister(5))
 	end
 
 	def test_cr_pass_6_returns_1_nickel_1_penny
-		assert_equal({Nickels: 1, Pennies: 1}, cashregister(6))
+		assert_equal({nickels: 1, pennies: 1}, cashregister(6))
 	end
 
 	def test_cr_pass_10_returns_1_dime
-		assert_equal({Dimes: 1}, cashregister(10))
+		assert_equal({dimes: 1}, cashregister(10))
 	end
 
 	def test_cr_pass_11_returns_1_dime_1_penny
-		assert_equal({Dimes: 1, Pennies: 1}, cashregister(11))
+		assert_equal({dimes: 1, pennies: 1}, cashregister(11))
 	end
 
 	def test_cr_pass_25_returns_1_quarter
-		assert_equal({Quarters: 1}, cashregister(25))
+		assert_equal({quarters: 1}, cashregister(25))
 	end
 
 	def test_cr_pass_19_returns_1_dime_1_Nickel_4_pennies
-		assert_equal({Dimes: 1, Nickels: 1, Pennies: 4}, cashregister(19))
+		assert_equal({dimes: 1, nickels: 1, pennies: 4}, cashregister(19))
 	end
 
 	def test_cr_pass_24_returns_2_dime_0_Nickel_4_pennies
-		assert_equal({Dimes: 2, Pennies: 4}, cashregister(24))
+		assert_equal({dimes: 2, pennies: 4}, cashregister(24))
 	end
 
 	def test_cr_pass_29_returns_1_Quarter_4_pennies
-		assert_equal({Quarters: 1, Pennies: 4}, cashregister(29))
+		assert_equal({quarters: 1, pennies: 4}, cashregister(29))
 	end
 
 end
